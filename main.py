49d089997e6831d7f873a68bdf1eb5c7b80b11a4
Nicolas Guillon <nicolas.guillon95@yahoo.com>
2024-12-18 11:09:00 +0100

    update board_game

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 from players import Player
+from cards import *
+import random
 
-board_game=["YELLOW CAMEMBERT" ,"BLUE", "BLUE CAMEMBERT", "YELLOW"]
+board_game=["bleu", "jaune", "vert", "bleu_camembert", "bleu", "jaune", "vert", "jaune_camembert", "bleu", "jaune", "vert", "vert_camembert"]
+
 
 
 player = Player("player1")
 
-player.move()
-if player.position == 2:
-    card1 = Card(blue)
-    card1.blue_question()
-    response = input()
-    if response == card1.blue_response():
-         player.add_blue_camembert()
+while player.camemberts != [1,1,1]:
+    print(board_game)
+    print("Score du joueur: " + player.camemberts)
+    print("Nombre de tours: " + player.nb_of_turns)
+
+    dice = random.randint(1,6)
+    print("Valeur du dés: " + dice)
+    player.move(dice)
+
+    case = board_game[player.position].split("_")
+
+    is_correct = played_card(choose_card(case[0]))
+
+    if is_correct:
+        if len(case) > 1:
+            if case[0] == "bleu":
+                player.add_blue_camembert
+            elif case[0] == "jaune":
+                player.add_yellow_camembert
+            # add function green color
+    else:
+        player.nb_of_turns += 1
